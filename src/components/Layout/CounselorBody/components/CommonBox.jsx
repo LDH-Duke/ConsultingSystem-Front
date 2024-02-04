bbd6d9cb89d13e9bb0e6946a9453d2fff0ea1bec
Donghee Lee <ehdgml506@naver.com>
2024-02-04 17:26:41 +0900

    feat : calendar working

--- src/components/Layout/CounselorBody/components/CommonBox.jsx
+++ src/components/Layout/CounselorBody/components/CommonBox.jsx
@@ -3,6 +3,8 @@ import './CommonBox.css'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+
+/* options -> height(높이), color(테두리,배경색), data: 내용 및 이미지 데이터 */
 const CommonBoxContainer = styled.div`
     width: 100%;
     height: ${(props) => props.height ? props.height : '100px'} ;
@@ -11,8 +13,8 @@ const CommonBoxContainer = styled.div`
     border: 1px solid ${(props) => props.color ? props.color : '#00cc8f'};
     border-radius: 5px;
 
-    background-color:${(props) => props.color ? props.color : '#00cc8f'};
-    box-shadow:0px 0px 20px #BEC2C9;
+    background-color:${(props) => props.color ? props.color : '#ffffff'};
+    /* box-shadow:0px 0px 20px #BEC2C9; */
 `
 
 const CommonBoxLink = styled(Link)`
--- src/components/Layout/TestCounselorMainPresenter.jsx
+++ src/components/Layout/TestCounselorMainPresenter.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Layout from './Layout'
 import { Banner } from './Banner/Banner'
 import { CounselorBodyLayout } from './CounselorBody/CounselorBodyLayout'
+import { Calendar } from './Calendar/Calendar'
 
 export const TestCounselorMainPresenter = () => {
   return (
     <Layout nav={0}>
-        <Banner />
-        <CounselorBodyLayout />
+      {/* <Banner /> */}
+      <Calendar />
+      <CounselorBodyLayout />
     </Layout>
   )
 }
